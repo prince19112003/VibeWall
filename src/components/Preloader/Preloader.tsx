282fd9be1f802b9f25a38fe97d385f812313a872
Prince Thakur <prince19112003@outlook.com>
2026-05-11 02:27:51 +0530

    fix: next/image imports

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -2,6 +2,7 @@
 
 import { motion, AnimatePresence } from 'framer-motion';
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 import styles from './Preloader.module.css';
 
 export default function Preloader() {
